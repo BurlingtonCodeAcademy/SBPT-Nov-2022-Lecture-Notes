998f937137719532eab9d1bfb0e736de26c6db12
Eric Winebrenner <ejwinebrenner@gmail.com>
2022-11-30 19:37:24 -0500

    operator notes added

---
+++ unit-2/labs/in_class/variables.js
@@ -0,0 +1,24 @@
+/* 
+!   Challenge:
+    Create 6 different variables. Each one will hold different data types.
+        - variable will be detail:
+            - First Name - holds a string
+            - Last Name - holds a string
+            - Birth Year - holds an integer
+            - Needs Coffee - holds a boolean
+            - The Current Year - holds an integer
+            - Age - holds an integer.
+                - This should reflect the difference between the current year and birth year. Not a literal.
+
+        These values can be fictional.
+
+            - Console Log Age.   
+*/
+
+let first = 'Ben';
+let last = "Rivera";
+let birth = 1999;
+let coffee = true;
+let currentYear = 2022;
+let age = currentYear - birth;
+console.log(age);
---
+++ unit-2/operators.js
@@ -0,0 +1,83 @@
+/* 
+    Operators
+        - Plus          +
+        - Times         *
+        - Power         **
+        - Dot           .
+        - Assignment    =
+        - Comparison    ==
+                        ===
+        
+        Used to:
+            - Assign
+            - Compare
+            - Arithmetic
+            - Logical
+            - Conditional
+
+    Expression & Statement
+
+        keyword name (express) {
+            ... code block
+        }
+*/
+
+// Equal To
+"3" == 3;
+console.log('3' == 3);
+
+// Strict Equal To
+console.log("3" === 3);
+
+// Not Equal & Strict Not Equal
+console.log('2' != 2);
+console.log('2' !== 2);
+
+// Greater than / Less than
+let y = 3 > 2;
+console.log('Greater Than: ', y);
+2 < 3;
+
+// Greater than or Equal to
+3 >= 2;
+
+// Less than or Equal to
+2 <= 2;
+
+// And
+2 && 2;
+2 < 10 && 2 > 1
+// console.log('And', 2 && 3);
+
+// Or 
+2 || 4;
+
+//* Logical Operators
+
+// let a = 1;
+// console.log(a); // 1
+// a = a + 1;
+// console.log(a); // 2
+// a = a + 1;
+// console.log(a); // 3
+
+// addition
+r += 1; // r = r + 1;
+
+// subtraction
+r -= 1; // r = r - 1;
+
+// multiplication
+r *= 2; // r = r * 2;
+
+// division
+r /= 2; // r = r / 2;
+
+// Remainder (modulus) 
+r %= 1;
+
+console.log(3 % 2);
+
+// Exponential
+r **= 1;
+
