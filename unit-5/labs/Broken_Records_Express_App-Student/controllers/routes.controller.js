2b26cb367c3538048e4e04a4baf737b1baea83ea
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-01-21 16:44:37 -0500

    broken records challenge solution added

--- unit-5/labs/Broken_Records_Express_App-Student/controllers/routes.controller.js
+++ unit-5/labs/Broken_Records_Express_App-Student/controllers/routes.controller.js
@@ -4,17 +4,15 @@ const fs = require('fs');
 const fsPath = './assets/db.json';
 
 router.get('/check', async(req, res) => {
-    res.sent('Got Endpoint hit!');
+    res.send('Got Endpoint hit!');
 });
 
 router.get('/all-from-database', async(req,res) => {
     try {
-        
-        
-        
+
         res.status(200).json({
             message: `Records Obtained!`,
-            results
+            results: db
         });
 
     } catch (err) {
@@ -29,9 +27,10 @@ router.get('/:id', async(req,res) => {
         const id = req.params.id;
         
         let result = db.filter(i => i.id == id);
-
+        
         if(result.length > 0) {
             res.status(200).json({
+                result
             });
         } else {
             res.status(404).json({
@@ -40,91 +39,126 @@ router.get('/:id', async(req,res) => {
         }
 
     } catch (err) {
-        status(500).send({
+        res.status(500).send({
             message: `Error: ${err}`
         });
     }
 });
 
-// router.post('/',  async(req, res) => {
-//     try {
+router.post('/',  async(req, res) => {
+    try {
         
-//         const title = req.body;
-//         const id = db.length + 1;
+        const title = req.body;
+        const id = db.length + 1;
 
-//         const obj = {
-//             id: id,
-//             title: title,
-//         }
+        const obj = {
+            id: id,
+            title: title.title,
+        }
 
-//         fs.readFile(fsPath, (err, data) => {
+        fs.readFile(fsPath, (err, data) => {
             
+            if(err) console.log(err);
+
+            const db = JSON.parse(data);
+            db.push(obj);
+
+            fs.writeFile(fsPath, JSON.stringify(db), err => console.log(err));
+
+            res.status(200).json({
+                message: "Record Added!",
+                obj
+            });
+        })
 
 
-//             const db = JSON.parse(data);
-//             db.push();
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+});
 
-//             fs.writeFile(fsPath, db, err => console.log(err));
-
-//             res.status(200).json({
-//                 message: "Record Added!",
-//                 obj
-//             });
-//         })
-
-
-//     } catch (err) {
-//         res.status(500).json({
-//             message: err.message,
-//         })
-//     }
-// });
-
-// router.put('/id', async(req,res) => {
-//     try {
+router.put('/:id', async(req,res) => {
+    try {
         
-//         const id = Number(req.body.id);
+        const id = Number(req.params.id);
 
-//         const { title } = req;
+        const { title } = req.body;
 
-//         fs.read(fsPath, (err, data) => {
+        fs.readFile(fsPath, (err, data) => {
 
-//             if(err) console.log(data);
+            if(err) console.log(data);
 
-//             const db = JSON.parse(data);
+            const db = JSON.parse(data);
+            // db = [ {"id:...."}]
 
-//             let updatedRecord;
+            let updatedRecord;
 
-//             db.forAll((r,i) => {
+            db.forEach((r,i) => {
 
-//                 if(r.id === id) {
-//                     db[0] = {
-//                         id,
-//                         title
-//                     };
+                if(r.id === id) {
+                    db[i] = {
+                        id,
+                        title
+                    };
 
-//                     updatedRecord = db[i];
-//                     fs.writeFile(fsPath, JSON.stringify(db), err => console.log(err));
-//                 }
+                    updatedRecord = db[i];
+                    fs.writeFile(fsPath, JSON.stringify(db), err => console.log(err));
+                }
 
-//             });
+            });
 
-//             updated ? 
-//                 res.status(200).json({
-//                     status: `Record ${id} updated.`,
-//                     updatedRecord
-//                 }) : 
-//                 res.status(404).json({
-//                     status: `ID ${id} not in database.`
-//                 });
+            updatedRecord ? 
+                res.status(200).json({
+                    status: `Record ${id} updated.`,
+                    updatedRecord
+                }) : 
+                res.status(404).json({
+                    status: `ID ${id} not in database.`
+                });
 
-//         });
+        });
 
-//     } catch (err) {
-//         res.status(500).json({
-//             status: `Error: ${err.message}`
-//         });
-//     }
-// });
+    } catch (err) {
+        res.status(500).json({
+            status: `Error: ${err.message}`
+        });
+    }
+});
+
+router.delete('/:id', async(req,res) => {
+    try {
+        
+        const id = Number(req.params.id);
+
+        fs.readFile(fsPath, (err, data) => {
+            if(err) throw err;
+
+            const db = JSON.parse(data);
+            console.log(db.length)
+            const keptRecords = db.filter(i => i.id != id);
+            console.log(keptRecords.length)
+            // console.log(keptRecords);
+
+            fs.writeFile(fsPath, JSON.stringify(keptRecords), err => console.log(err));
+
+            if(keptRecords.length < db.length) {
+                res.status(200).json({
+                    status: "Successfully Deleted!"
+                })
+            } else {
+                res.status(404).json({
+                    status: "No Record Found"
+                })
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        })
+    }
+});
 
 module.exports = router;
